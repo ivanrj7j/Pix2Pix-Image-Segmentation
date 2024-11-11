d1aa8484268d405bb37f04e7f1da5b8036e1b0e8
ivanrj7j <ivanrj7j@gmail.com>
2024-11-11 18:42:02 +0530

    added config

---
+++ config.py
@@ -0,0 +1,28 @@
+from uuid import uuid4
+import os
+
+trainingID = str(uuid4())
+training = False
+# training conigurations 
+
+savePath = os.path.join("saves", trainingID)
+previewPath = os.path.join("preview", trainingID)
+# path configuration 
+
+os.makedirs(savePath, exist_ok=True)
+os.makedirs(previewPath, exist_ok=True)
+# Create directories if they don't exist
+
+savedDiscriminator = ""
+saveGenerator = ""
+# save path of the discriminator and generator to load from 
+
+epochs = 100
+lr = 2e-4
+batchSize = 32
+betas = (0.5, 0.999)
+l1Lambda = 100
+# training loop configuration 
+
+if training:
+    print(f"Training ID: {trainingID}")
