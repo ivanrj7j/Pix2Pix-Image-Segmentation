969df00d48f983b82985d4c8ac2df2e0532b4834
ivanrj7j <ivanrj7j@gmail.com>
2024-11-11 19:24:49 +0530

    working on training the model

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ os.makedirs(previewPath, exist_ok=True)
 # Create directories if they don't exist
 
 savedDiscriminator = ""
-saveGenerator = ""
+savedGenerator = ""
 # save path of the discriminator and generator to load from 
 
 epochs = 100
@@ -22,9 +22,23 @@ lr = 2e-4
 batchSize = 32
 betas = (0.5, 0.999)
 l1Lambda = 100
+generatorLRDecay = 0.999
+discriminatorLRDecay = 0.99
 # training loop configuration 
 
 device = "cuda"
+
+trainingFeaturePath = "Human-Segmentation-Dataset/Training_Images"
+trainingTargetPath = "Human-Segmentation-Dataset/Ground_Truth"
+testFeaturePath = "test dataset/inputImage"
+testTargetPath = "test dataset/outputImage"
+# defining paths to data 
+
+resolution = (128, 128) 
+# image resolution 
+
+decayEvery = 2
+saveEvery = 5
 
 if training:
     print(f"Training ID: {trainingID}")
--- train.py
+++ train.py
@@ -1,10 +1,102 @@
-from models.discriminator import Discriminator
-from models.generator import Generator
+from utils import saveImage, saveModel, loadModels
 from loaders.dataset import getDataLoader
 # importing custom code 
 
 import torch
 from torch.optim.adam import Adam
 from torch.optim.lr_scheduler import ExponentialLR
-from torch.nn import L1Loss
+from torch.nn import L1Loss, BCEWithLogitsLoss
 # importing torch related stuff
+
+import config
+# importing configurations for training
+
+from tqdm import tqdm
+# importing non torch stuff 
+
+generator, discriminator = loadModels(config.savedGenerator, config.savedDiscriminator, config.device)
+# loading models 
+
+generatorOptim = Adam(generator.parameters(), config.lr, config.betas)
+discOptim = Adam(discriminator.parameters(), config.lr, config.betas)
+# initalizing the optimizers 
+  
+schedulerG = ExponentialLR(generatorOptim, gamma=config.generatorLRDecay)
+schedulerD = ExponentialLR(discOptim, gamma=config.discriminatorLRDecay)
+# initialzing schedulers 
+
+l1 = L1Loss()
+bce = BCEWithLogitsLoss()
+# initializing losses 
+
+trainLoader = getDataLoader(config.trainingFeaturePath, config.trainingTargetPath, config.resolution, config.batchSize)
+testLoader = getDataLoader(config.testFeaturePath, config.testTargetPath, config.resolution, config.batchSize)
+# initializing data loaders 
+
+def step(x:torch.Tensor, y:torch.Tensor) -> tuple[float, float]:
+    generatedImage = generator.forward(x)
+
+    discReal = discriminator.forward(x, y)
+    discFake = discriminator.forward(x, generatedImage.detach())
+
+    zeroNoise = torch.zeros_like(discFake) + (torch.rand_like(discFake) * 0.05)
+    oneNoise = torch.ones_like(discReal) - (torch.rand_like(discReal) * 0.05)
+
+    discLoss = (bce(discReal, oneNoise) + bce(discFake, zeroNoise))/2
+    # calculating the loss of discriminator 
+
+    discriminator.zero_grad()
+    discOptim.zero_grad()
+    discLoss.backward()
+    discOptim.step()
+
+    criticLoss = discriminator.forward(x, generatedImage)
+    l1Loss = l1(generatedImage, y)
+    genLoss = criticLoss + (config.l1Lambda * l1Loss)
+    # calculating generator loss 
+
+    generator.zero_grad()
+    generatorOptim.zero_grad()
+    genLoss.backward()
+    generatorOptim.step()
+
+    return genLoss.item(), discLoss.item()
+
+def train():
+    for epoch in range(1, config.epochs+1):
+        generator.train()
+        discriminator.train()
+
+        loader = tqdm(trainLoader, f"[{epoch}/{config.epochs}]", len(trainLoader))
+        genLoss, discLoss = 0, 0
+        for x, y in loader:
+            x, y = x.to(config.device), y.to(config.device)
+            genLoopLoss, discLoopLoss = step(x, y)
+            genLoss += genLoopLoss / len(trainLoader)
+            discLoss += discLoopLoss / len(trainLoader)
+
+            loader.set_postfix({"genLoss": genLoss, "discLoss": discLoss, "lrG":schedulerG.get_lr(), "lrD": schedulerD.get_lr()})
+
+        if epoch % config.decayEvery == 0:
+            schedulerG.step()
+            schedulerD.step()
+            # decaying the learning rates 
+
+        if epoch % config.saveEvery == 0:
+            saveModel(generator, config.savePath, f"gen_{epoch}")
+            saveModel(discriminator, config.savePath, f"disc_{epoch}")
+            # saving models every saveEvery epochs
+
+        for x, y in testLoader:
+            x = x.to(config.device)
+            image = generator.forward(x)
+            saveImage(image, config.previewPath, f"preview_{epoch}")
+            break
+            # testing the models on the test set
+
+    saveModel(generator, config.savePath, "gen_final")
+    saveModel(discriminator, config.savePath, "dis_final")
+
+
+if __name__ == "__main__":
+    train()
