556a791a6108af0482f0be538788c1167b9d5b85
ivanrj7j <ivanrj7j@gmail.com>
2024-11-16 18:48:58 +0530

    optimized the data loader

--- loaders/dataset.py
+++ loaders/dataset.py
@@ -3,8 +3,10 @@ import os
 import torch
 from PIL import Image
 import torchvision.transforms as transforms
+from albumentations.pytorch.transforms import ToTensorV2
 import albumentations as A
 import numpy as np
+import cv2
 
 class SegmentationDataset(Dataset):
     def __init__(self, featurePath:str, targetPath:str, resolution:tuple[int, int]=(256, 256)) -> None:
@@ -23,8 +25,7 @@ class SegmentationDataset(Dataset):
         # initializing the resolution
 
         self.trans1 = transforms.Compose([
-            transforms.Resize(self.resolution),
-            np.array
+            transforms.Resize(self.resolution)
         ])
         # transformations to the image 
 
@@ -37,13 +38,10 @@ class SegmentationDataset(Dataset):
             A.RandomFog(p=0.3),
             A.MotionBlur(p=0.1),
             A.GaussianBlur(p=0.1),
-            A.GaussNoise((0, 0.3))
+            A.GaussNoise((0, 0.3)),
+            A.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5), max_pixel_value=1),
+            ToTensorV2(),
         ], additional_targets={'mask': 'mask'})  
-
-        self.trans3 = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(0.5, 0.5, 0.5)
-        ])
 
     def __len__(self) -> int:
         return len(self.featureImages)
@@ -53,19 +51,16 @@ class SegmentationDataset(Dataset):
         targetImagePath = os.path.join(self.targetPath, self.targetImages[idx])
         # getting the path to the images 
 
-        featureImage = Image.open(featureImagePath).convert("RGB")
-        targetImage = Image.open(targetImagePath).convert("L")
+        featureImage = cv2.resize(cv2.imread(featureImagePath), self.resolution)
+        targetImage = cv2.resize(cv2.imread(targetImagePath), self.resolution)
 
-        featureImage = self.trans1(featureImage)
-        targetImage = self.trans1(targetImage)
+        featureImage = cv2.cvtColor(featureImage, cv2.COLOR_BGR2RGB)
+        targetImage = cv2.cvtColor(targetImage, cv2.COLOR_BGR2GRAY)
         # applying the transformations to the images
 
         augmented = self.trans2(image=featureImage, mask=targetImage)
         featureImage = augmented['image']
         targetImage = augmented['mask']
-
-        featureImage = self.trans3(featureImage)
-        targetImage = self.trans3(targetImage)
 
         return featureImage, targetImage
     
